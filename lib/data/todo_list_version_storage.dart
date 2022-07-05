060c5b9e104b470c099cf829082940cca6acdd4c
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2022-07-05 08:37:15 +0700

    Update Future readVersionList and Future saveVersion

--- lib/data/todo_list_version_storage.dart
+++ lib/data/todo_list_version_storage.dart
@@ -17,7 +17,10 @@ class FileSystemTodoListVersionStorage extends TodoListVersionStorage{
   @override
   Future<List<TodoListVersion>> readVersionList() async {
     try {
-
+      final file = await _localFile;
+      final content = await file.readAsString();
+      final List<dynamic> jsonArray = jsonDecode(content);
+      return jsonArray.map((jsonObject) => TodoListVersion.fromJson(jsonObject),).toList();
     }catch(e){
       return [];
     }
@@ -26,5 +29,18 @@ class FileSystemTodoListVersionStorage extends TodoListVersionStorage{
     final directory = await getApplicationDocumentsDirectory();
     final path = directory.path;
     return File('$path/todoListVersionList.json');
+  }
+
+  @override
+  Future<void> saveVersion(List<Todo> todoList) async {
+    final lastestTodoListVersionList = await readVersionList();
+    lastestTodoListVersionList.add(TodoListVersion(createdAt: DateTime.now().millisecondsSinceEpoch, todos: todoList));
+    try{
+      final file = await _localFile;
+      final jsonArray = lastestTodoListVersionList.map((e)=> e.toJson(),).toList();
+      await file.writeAsString(jsonEncode(jsonArray));
+    }catch (e){
+      return;
+    }
   }
 }
