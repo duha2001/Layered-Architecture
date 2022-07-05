f55fc12e49dae8daa8188f8b7c4e565e63062c09
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2022-07-05 08:49:58 +0700

    Create file user_mode_storage

---
+++ lib/data/user_mode_storage.dart
@@ -0,0 +1,18 @@
+enum UserMode {normal, sandbox, superSandbox}
+
+abstract class UserModeStorage{
+  UserMode? get currentUserMode;
+  set currentUserMode(UserMode? currentUserMode);
+}
+
+class CacheUserModeStorage extends UserModeStorage{
+  UserMode? _currentUserMode = UserMode.normal;
+
+  @override
+  UserMode? get currentUserMode => _currentUserMode;
+
+  @override
+  set currentUserMode(UserMode? currentUserMode){
+    _currentUserMode = currentUserMode;
+  }
+}
