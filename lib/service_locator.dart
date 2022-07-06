780be213b89261839261345cbf2ecc88f01d6429
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2022-07-06 23:01:58 +0700

    add all package on file service_locator

---
+++ lib/service_locator.dart
@@ -0,0 +1,5 @@
+import 'package:layered_architecture/business/sandbox_todo_bloc.dart';
+import 'package:layered_architecture/business/super_sandbox_todo_bloc.dart';
+import 'package:layered_architecture/business/todo_bloc.dart';
+import 'package:layered_architecture/business/todo_list_version_bloc.dart';
+import 'package:layered_architecture/data/todo_storage.dart';
