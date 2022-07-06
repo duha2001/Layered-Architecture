613e007b7cb0086e2fd3ac06b0c490da1c8b68a1
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2022-07-06 23:37:03 +0700

    Create fuction setupMainDeps

--- lib/service_locator.dart
+++ lib/service_locator.dart
@@ -3,3 +3,18 @@ import 'package:layered_architecture/business/super_sandbox_todo_bloc.dart';
 import 'package:layered_architecture/business/todo_bloc.dart';
 import 'package:layered_architecture/business/todo_list_version_bloc.dart';
 import 'package:layered_architecture/data/todo_storage.dart';
+import 'package:layered_architecture/data/todo_list_version_storage.dart';
+import 'package:layered_architecture/data/user_mode_storage.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt = GetIt.instance;
+
+const kFileSystem = 'fileSystem';
+const kCached = 'cached';
+
+void setupMainDeps(){
+  getIt.registerSingleton<TodoStorage>(
+    FileSystemTodoStorage(),
+    instanceName: kFileSystem,
+  );
+}
