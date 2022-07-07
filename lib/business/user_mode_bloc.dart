60f4c5c74ef8b1266ef661fbccfcab1130391182
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2022-07-07 14:49:34 +0700

    Create file user_mode_bloc

---
+++ lib/business/user_mode_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:layered_architecture/data/user_mode_storage.dart';
+import 'package:layered_architecture/service_locator.dart';
+
+class UserModeBloc{
+  final UserModeStorage _userModeStorage;
+
+  UserModeBloc({required UserModeStorage userModeStorage})
+    : _userModeStorage = userModeStorage;
+
+  bool startUserMode(UserMode? userMode){
+    final currentUserMode = _userModeStorage.currentUserMode;
+    if (currentUserMode == userMode) return false;
+    getIt.pushNewScope(scopeName: userMode.toString());
+    switch(userMode){
+      case UserMode.sandbox:
+        setupSandBoxDeps();
+        break;
+      case UserMode.superSandbox:
+        setupSuperSandboxDeps();
+        break;
+      default:
+    }
+    _userModeStorage.currentUserMode = userMode;
+    return true;
+  }
+
+  void exitUserMode(){
+    getIt.popScope();
+    _userModeStorage.currentUserMode = UserMode.normal;
+  }
+}
