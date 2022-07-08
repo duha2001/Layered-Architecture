ffb37f6ca1a4a3769e989deb356cd1d5ecf60917
DESKTOP-AUNIITC\anhdu <duha2001@github.com>
2022-07-08 10:26:49 +0700

    Create new file sandbox_home_page

---
+++ lib/presentation/sandbox_home_page.dart
@@ -0,0 +1,63 @@
+import 'package:layered_architecture/business/sandbox_todo_bloc.dart';
+import 'package:layered_architecture/business/user_mode_bloc.dart';
+import 'package:layered_architecture/data/todo_storage.dart';
+import 'package:layered_architecture/presentation/widgets/todo_list_view.dart';
+import 'package:layered_architecture/service_locator.dart';
+import 'package:flutter/material.dart';
+
+import 'package:layered_architecture/presentation/widgets/add_todo_button.dart';
+class SandboxHomePage extends StatefulWidget{
+  const SandboxHomePage({Key? key}):super(key:key);
+  @override
+  State<SandboxHomePage> createState() => _SandboxHomePageState();
+
+}
+class _SandboxHomePageState extends State<SandboxHomePage>{
+  final TodoStorage _todoStorage = getIt.get(instanceName: kCached);
+  final UserModeBloc _userModeBloc = getIt.get();
+  final SandboxTodoBloc _todoBloc= getIt.get();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepOrange[300],
+      appBar: AppBar(
+        centerTitle: true,
+        title: const Text('Sanbox'),
+        leading: BackButton(
+          onPressed: (){
+            _userModeBloc.exitUserMode();
+            Navigator.of(context).maybePop();
+          },
+        ),
+        actions: [
+          IconButton(
+              onPressed: (){
+                _todoBloc.save();
+                _userModeBloc.exitUserMode();
+                Navigator.of(context).maybePop();
+              },
+              icon: const Icon(Icons.save),
+          )
+        ],
+      ),
+      body: SafeArea(
+        child: Stack(
+          children: [
+            TodoListView(todoStorage: _todoStorage),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: AddTodoButton(
+                addTodo: _todoBloc.addTodo,
+                onAddedSuccess: (){
+                  setState((){});
+                },
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+}
